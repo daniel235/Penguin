84e21e472c98e3205245b8860d69e74ae78b673a
daniel235 <daduncan298@gmail.com>
2020-04-08 01:30:27 -0400

    update

--- testFiles/test_script.py
+++ testFiles/test_script.py
@@ -3,12 +3,15 @@ import h5py
 #test to check if required files are created
 def file_test(bed_file, ref_file, sam_file):
     if bed_file == None:
+	print("bed file missing")
         raise FileNotFoundError
 
-    elif ref_file == None:
+    elif ref_file == None and sam_file == None:
+	print("ref file missing")
         raise FileNotFoundError
 
     elif sam_file == None:
+	print("sam file missing")
         raise FileNotFoundError
 
     return
