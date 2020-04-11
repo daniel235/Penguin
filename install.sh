90af39209f00e365cdd794c8b6ac09d7aed54581
daniel235 <daduncan298@gmail.com>
2020-04-11 00:58:31 -0400

    adding 1 folder data

--- install.sh
+++ install.sh
@@ -8,6 +8,6 @@ git clone https://github.com/Psy-Fer/SquiggleKit.git
 pip install numpy h5py sklearn matplotlib
 pip install scrappie
 pip3 install --user --upgrade tensorflow 
-
+pip install keras
 #create empty directory for data
 mkdir Data/basecall/
