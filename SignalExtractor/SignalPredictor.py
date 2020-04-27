f390053ed5f91958cafac76e4bac94dba33b4fc6
daniel235 <daduncan298@gmail.com>
2020-04-27 18:30:22 -0500

    update

--- SignalExtractor/SignalPredictor.py
+++ SignalExtractor/SignalPredictor.py
@@ -32,9 +32,10 @@ def get_sam_data(sam):
     with open(sam, 'r') as f:
         for line in f:
             column = line.split( )
-            if '@' not in column[0]:
-                #add read id/chromosome number/location/cigar/sequence
-                sam_data.append([column[0], column[2], column[3], column[4], column[9]])
+            if len(column) > 0:
+                if '@' not in column[0]:
+                    #add read id/chromosome number/location/cigar/sequence
+                    sam_data.append([column[0], column[2], column[3], column[4], column[9]])
 
     return sam_data
 
