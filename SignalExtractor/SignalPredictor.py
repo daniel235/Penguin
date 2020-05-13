520b54a77ff88eba0255b14eca246ea2bcae0dc1
daniel235 <daduncan298@gmail.com>
2020-05-13 14:33:09 -0500

    update

--- SignalExtractor/SignalPredictor.py
+++ SignalExtractor/SignalPredictor.py
@@ -218,7 +218,7 @@ def nanopolish_predict(model, eventAlign, fastpath, bedPath, samPath, IdFile, on
     #conver kmers
     hot_kmers = PD.createEncoder(kmers)
     new_hot_kmers = PD.nano_to_onehot(data)
-    data["reference_kmer"] = new_hot_kmers
+    data["reference_kmer"] = hot_kmers
 
     print("ref ", data["reference_kmer"])
 
