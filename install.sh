0eaf8798d4d4e29ecc671518b84a7597eacb0928
daniel235 <daduncan298@gmail.com>
2020-07-29 15:21:47 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ sudo apt-get install build-essential
 sudo apt-get install libz-dev
 sudo apt-get install libbz2-dev
 sudo apt-get install libncurses5-dev
+sudo apt-get install -y liblzma-dev
 
 #install minimap2
 git clone https://github.com/lh3/minimap2
